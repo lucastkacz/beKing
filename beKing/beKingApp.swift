a934e99f34ba934f8e28e88b828cc913ebb42972
lucas <lucastkacz@gmail.com>
2025-11-11 15:02:36 -0300

    Initial Commit

---
+++ beKing/beKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  beKingApp.swift
+//  beKing
+//
+//  Created by Lucas Tkacz on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct beKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
